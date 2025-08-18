ce5fea5074923c95f6d657542e034c11d775b2e5
Matianxing <450086055@qq.com>
2025-08-18 09:31:09 +0800

    feat: 跨域与接口配置

---
+++ backend/src/main/java/com/pony/backend/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.pony.backend.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true).
+                allowedOriginPatterns("*").
+                allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").
+                allowedHeaders("*").
+                exposedHeaders("*");
+    }
+}
---
+++ backend/src/main/java/com/pony/backend/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.pony.backend.controller;
+
+import com.pony.backend.common.BaseResponse;
+import com.pony.backend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
